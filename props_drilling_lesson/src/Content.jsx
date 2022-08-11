df0f805a585636405db76b6d8413c86e9c241bd6
Jaejin Youn <jaejinyoun@Jaejins-MacBook-Pro.local>
2022-08-11 17:22:12 +0900

    completed props-drilling

--- props_drilling_lesson/src/Content.jsx
+++ props_drilling_lesson/src/Content.jsx
@@ -1,62 +1,20 @@
 import React, { useState } from 'react';
-import { FaTrash } from 'react-icons/fa';
+import ItemList from './ItemList';
 
-const Content = () => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound of Cocoa Covered Almonds Unsalted",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Two half pound of Cocoa Covered Almonds Unsalted",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Three half pound of Cocoa Covered Almonds Unsalted",
-    }
-  ])
+const Content = ({items, handleCheck, handleDelete}) => {
 
-  const handleCheck = (id) => {
-    const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item)
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-  }
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.removeItem('shoppinglist', JSON.stringify(listItems));
-  }
 
   return (
     <main>
       {items.length ? (
-        <ul>
-          {items.map((item) => (
-            <li className='item' key={item.id}>
-              <input
-                type="checkbox"
-                onChange={() => handleCheck(item.id)}
-                checked={item.checked}
-              />
-              <label
-                style={(item.checked) ? {textDecoration: 'line-through'} : null}
-                onDoubleClick={() => handleCheck(item.id)}
-              >{item.item}</label>
-              <FaTrash 
-                onClick={() => handleDelete(item.id)}
-              role="button" tabIndex="0" />
-            </li>
-          ))}
-        </ul>
-        ) :  (
-          <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
-        )
-      }
+        <ItemList 
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+      )}
     </main>
   );
 };
--- props_drilling_lesson/src/Footer.jsx
+++ props_drilling_lesson/src/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Footer = () => {
+const Footer = ({length}) => {
   const today = new Date();
   return (
     <footer>
+      {/* <p>{length} List {length === 1 ? "item" : "items"}</p> */}
+      <p>
+        {length === 0 ? "No item" : 
+          length === 1 ? length + " List item" :
+          length + " List items"
+        }
+      </p>
       <p>Copyright &copy; {today.getFullYear()}</p>
     </footer>
   );
